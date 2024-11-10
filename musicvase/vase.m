d6512117373c4e08eca7c3ffa52222ca250bc081
RaduTek <me@radutek.uk>
2024-11-10 11:42:25 +0200

    Add vase drawing function and audio visualizer with RMS values

---
+++ musicvase/vase.m
@@ -0,0 +1,32 @@
+function vase(r, R, n, N, h)
+%VASE Draws a vase
+%   r = small radius
+%   R = big radius
+%   n = number of sides
+%   N = number of vase slices
+%   h = total height
+
+hp = h / N;     % height of each slice
+
+hold("on");
+
+for i = 0:(N-1)
+    k = mod(i, 3);
+
+    if k == 0
+        vv = [R R];
+    elseif k == 1
+        vv = [R r];
+    elseif k == 2
+        vv = [r R];
+    end
+
+    [xp, yp, zp] = cylinder(vv, n);
+    zp = (zp + i) * hp;
+    surf(xp, yp, zp);
+end
+
+hold("off");
+
+end
+
